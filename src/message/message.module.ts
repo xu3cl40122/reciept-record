54d53e9fbcea7ac1f1feb9638f289c4d06a41824
tom lee <xu3cl40122@gmail.com>
2021-07-08 21:18:00 +0800

    Fixed original file structure

--- src/message/message.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MessageService } from './message.service';
-
-@Module({
-  providers: [MessageService],
-  exports: [MessageService]
-})
-export class MessageModule {}
